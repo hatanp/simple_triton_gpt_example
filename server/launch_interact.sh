d396fdc22893d09b88e51f93af3532ba747aa587
Väinö Hatanpää <vaino.hatanpaa@csc.fi>
2023-03-08 12:49:42 +0000

    Added interactive server launch script

---
+++ server/launch_interact.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#This script is only used for running shell in docker to not lose status in development
+
+#Setting various env variables that might or might not be required
+export WORKDIR=/media/volume/cache/tmp/
+export HF_DATASETS_CACHE=/media/volume/cache/hf/datasets/
+export HF_METRICS_CACHE=/media/volume/cache/hf/metrics/
+export HF_MODULES_CACHE=/media/volume/cache/hf/modules/
+export HF_TRANSFORMERS_CACHE=/media/volume/cache/hf/transformers/
+export TRANSFORMERS_CACHE=/media/volume/cache/hf/transformers/
+export TMPDIR=/media/volume/cache/tmp
+
+sudo docker run --shm-size 1g -it --gpus=1 --rm -p8000:8000 -p8001:8001 -p8002:8002 -v/media/volume/triton/simple_triton_gpt_example/server/model_repository:/models nvcr.io/nvidia/tritonserver:23.01-py3 bash
+
+#Run following as you please
+#pip install transformers torch
+#tritonserver --model-repository=/models
